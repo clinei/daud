e57dd5ef6e44337033addf006eadfed9a81a0c8f
Tanel Tagaväli <tanel58@hotmail.com>
2016-02-21 19:19:34 +0200

    add sine and saw generators, update noise generator

--- source/app.d
+++ source/app.d
@@ -4,14 +4,40 @@ void main()
 	import daud.drivers.alsa : AudioDevice;
 	import deimos.alsa.pcm : snd_pcm_stream_t;
 	import deimos.alsa.pcm : snd_pcm_format_t;
-	auto device = scoped!AudioDevice(snd_pcm_stream_t.PLAYBACK, snd_pcm_format_t.FLOAT64, 1);
+	auto device = scoped!AudioDevice(snd_pcm_stream_t.PLAYBACK, snd_pcm_format_t.FLOAT);
 
-	import daud.gen : noise;
-	auto buf = noise!double(-22050, 22050, 2 ^^ 16);
+	size_t bufSize = 2 ^^ 16;
+
+	version(noise)
+	{
+		import daud.gen : noise;
+		auto ns = noise!float;
+
+		import std.range : take;
+		auto wave = ns.take(bufSize);
+	}
+	else version(wave)
+	{
+		uint freq = 440;
+
+		version(saw)
+		{
+			import daud.gen : saw;
+			auto form = saw!float(freq, 48_000);
+		}
+		else version(sine)
+		{
+			import daud.gen : sine;
+			auto form = sine!float(freq, 48_000);
+		}
+
+		import std.range : repeat;
+		import std.array : join;
+		auto wave = form.repeat(bufSize / form.length).join();
+	}
+
+	import std.array : array;
+	auto buf = wave.array;
 
 	device.write(buf);
-
-	import core.thread : Thread;
-	import core.time : seconds;
-	Thread.sleep(1.seconds);
 }
--- source/daud/gen.d
+++ source/daud/gen.d
@@ -1,15 +1,35 @@
 module daud.gen;
 
-T[] noise(T)(T lowerBound, T upperBound, size_t length)
+auto noise(T)()
 {
-	T[] buf;
-	buf.length = length;
-
 	import std.range : generate;
 	import std.random : uniform;
 	import std.range : take;
-	import std.algorithm : copy;
-	generate!(() => uniform!"[]"(lowerBound, upperBound)).take(buf.length).copy(buf[]);
+	return generate!(() => uniform!"[]"(T(-1), T(1)));
+}
 
-	return buf;
+auto sine(T)(uint frequency, ulong sampleRate)
+{
+	import std.math : PI;
+	import std.algorithm : map;
+	import std.math : sin;
+	return saw!T(frequency, sampleRate, 2 * PI).map!sin;
+}
+
+auto saw(T)(uint frequency, ulong sampleRate, T scale = 1)
+{
+	// bigger frequency => smaller period
+
+	import core.time : seconds;
+	auto period = 1.seconds / frequency;
+
+	// smaller period => bigger step
+
+	T step = 1.seconds.total!"hnsecs" / T(period.total!"hnsecs") / T(sampleRate);
+
+	import std.range : iota;
+	import std.math : PI;
+	import std.algorithm : map;
+	import std.math : sin;
+	return iota!(T, T, T)(0, scale, step * scale);
 }
